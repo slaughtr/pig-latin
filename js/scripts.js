803a02a642db66ad66b4aea40f06ba72d1d71276
Dallas Slaughter and Grace Stuart <grace@>
2017-02-22 13:44:13 -0800

    refactered user logic and added comments to all code.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@
 var pigLatinArray = []; // this is the empty array the translated strings will be entered into
 function singleVowelCheck(string) {
   var initialArray = string.split(" "); //takes the input string and makes each item seperated by a space into a single word within the array initialArray
-  initialArray.forEach(function(index) {
-    if (index.length === 1 || index[0] === "a" || index[0] === "e" || index[0] === "i" || index [0] === "o" || index [0] === "u") { //if word is single letter, or if word starts with a vowel, do the following function
+  initialArray.forEach(function(index) { //for each loop that checks every entry in the array created from the user input
+    if (index.length === 1 || index[0] === "a" || index[0] === "e" || index[0] === "i" || index[0] === "o" || index[0] === "u") { //if word is single letter, or if word starts with a vowel, do the following function
       index = index + "ay"; // take the word and add "ay" to the end of it.
       pigLatinArray.push(index); // take the new string and put it at the end of the pigLatinArray array
+      // else if (index[]) {
+      //
+      // }
     } // closes forEach function
   });
   return pigLatinArray; // takes the updated pigLatinArray array and sends it to the front-end logic
@@ -14,12 +17,12 @@ function singleVowelCheck(string) {
 // below this line is user, or front-end, logic
 $(document).ready(function() { //business logic does NOT need to be in doc.ready
 
+
   $("form#userInput").submit(function() {
     event.preventDefault();
     var value = $("#inputString").val();
-    var translatedValue = singleVowelCheck(value); // takes the inputed string and runs it through the back-end function, with output being the new var translatedValue
-    var translatedString = translatedValue.join(" "); //take translatedValue (which will be pigLatinArray, and joins it back together with a space between words)
+    singleVowelCheck(value); // runs the singleVowelCheck function with the input from the user as an argument
     $("#result").show();
-    $("#resultP").append(translatedString);
+    $("#resultP").append(pigLatinArray.join(" ")); //takes translated user input saved as pigLatinArray and joins it back together with spaces, displaying it
   }) //closes form.submit
 }); //closes doc.ready
