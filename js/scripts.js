60308316fe3e4747b26eb951e613920fa60e445a
Dallas Slaughter and Grace Stuart <grace@>
2017-02-22 13:04:24 -0800

    add comments to business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
 // business logic
 // debugger;
-var pigLatinArray = [];
+var pigLatinArray = []; // this is the empty array the translated strings will be entered into
 function singleVowelCheck(string) {
-  var initialArray = string.split(" ");
+  var initialArray = string.split(" "); //takes the input string and makes each item seperated by a space into a single word within the array initialArray
   initialArray.forEach(function(index) {
-    if (index.length === 1 || index[0] === "a" || index[0] === "e" || index[0] === "i" || index [0] === "o" || index [0] === "u") {
-      index = index + "ay";
-      pigLatinArray.push(index);
-    } if else {
-
-    }
-
+    if (index.length === 1 || index[0] === "a" || index[0] === "e" || index[0] === "i" || index [0] === "o" || index [0] === "u") { //if word is single letter, or if word starts with a vowel, do the following function
+      index = index + "ay"; // take the word and add "ay" to the end of it.
+      pigLatinArray.push(index); // take the new word and put it at the end of the pigLatinArray array
+    } // closes forEach function
   });
   return pigLatinArray;
 }; // closes alphacheck
