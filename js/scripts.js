c589ed4122ee6fd7d4d1164a7d8ae84a232d704e
Dallas Slaughter and Grace Stuart <dallas@>
2017-02-22 13:14:35 -0800

    add comments to user logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,25 @@
-// business logic
-// debugger;
+// below this line is business, or back-end, logic
 var pigLatinArray = []; // this is the empty array the translated strings will be entered into
 function singleVowelCheck(string) {
   var initialArray = string.split(" "); //takes the input string and makes each item seperated by a space into a single word within the array initialArray
   initialArray.forEach(function(index) {
     if (index.length === 1 || index[0] === "a" || index[0] === "e" || index[0] === "i" || index [0] === "o" || index [0] === "u") { //if word is single letter, or if word starts with a vowel, do the following function
       index = index + "ay"; // take the word and add "ay" to the end of it.
-      pigLatinArray.push(index); // take the new word and put it at the end of the pigLatinArray array
+      pigLatinArray.push(index); // take the new string and put it at the end of the pigLatinArray array
     } // closes forEach function
   });
-  return pigLatinArray;
-}; // closes alphacheck
+  return pigLatinArray; // takes the updated pigLatinArray array and sends it to the front-end logic
+}; // closes singleVowelCheck function
 
-$(document).ready(function() {
+// below this line is user, or front-end, logic
+$(document).ready(function() { //business logic does NOT need to be in doc.ready
 
   $("form#userInput").submit(function() {
     event.preventDefault();
     var value = $("#inputString").val();
-    var translatedValue = singleVowelCheck(value);
+    var translatedValue = singleVowelCheck(value); // takes the inputed string and runs it through the back-end function, with output being the new var translatedValue
     var translatedString = translatedValue.join(" "); //take translatedValue (which will be pigLatinArray, and joins it back together with a space between words)
     $("#result").show();
     $("#resultP").append(translatedString);
-  })
-
-// user logic
-
+  }) //closes form.submit
 }); //closes doc.ready
