d7b9fd878da027e6d0f2a5dd298626dbd0cbafd8
Dallas Slaughter and Grace Stuart <dallas@>
2017-02-22 12:52:36 -0800

    complete function to add ay to single letter word.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 // business logic
-debugger;
+// debugger;
+var pigLatinArray = [];
 function singleVowelCheck(string) {
   var initialArray = string.split(" ");
   initialArray.forEach(function(index) {
     if (index.length === 1) {
       index = index + "ay";
+      pigLatinArray.push(index);
+    } if else {
+        
     }
   });
+  return pigLatinArray;
 }; // closes alphacheck
 
 $(document).ready(function() {
@@ -14,10 +19,10 @@ $(document).ready(function() {
   $("form#userInput").submit(function() {
     event.preventDefault();
     var value = $("#inputString").val();
-
+    var translatedValue = singleVowelCheck(value);
+    var translatedString = translatedValue.join(" "); //take translatedValue (which will be pigLatinArray, and joins it back together with a space between words)
     $("#result").show();
-    console.log(singleVowelCheck(value));
-    $("p#result").text(singleVowelCheck(value));
+    $("#resultP").append(translatedString);
   })
 
 // user logic
