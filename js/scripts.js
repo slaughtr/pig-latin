b8f9700443909a75c79c456b2cca07c81b1009b9
Dallas Slaughter and Grace Stuart <dallas@>
2017-02-22 16:54:28 -0800

    Fix minor JS bugs. Still not ready. Does not work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,46 @@
 // below this line is business, or back-end, logic
-var pigLatinArray = []; // this is the empty array the translated strings will be entered into
-// var consonantsArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]; //array containing all consonants except q, including y
-var vowelsArray = ["a", "e", "i", "o", "u"]; // array containing all vowels except y
+var pigLatinArray = [];
+var vowelsArray = ["a", "e", "i", "o", "u"];
 function singleVowelCheck(string) {
-  var initialArray = string.split(" "); //takes the input string and makes each item seperated by a space into a single word within the array initialArray
-  initialArray.forEach(function(index) { //for each loop that checks every entry in the array created from the user input
-    // for (var i = 0; i <= vowelsArray.length; i++) {
-      if (index[0] === "a" || index[0] === "e" || index[0] === "i" || index[0] === "o" || index[0] === "u" || index.length === 1) {
-        //if word is single letter, or if word starts with a vowel, do the following function
-        index = index + "ay"; // take the word and add "ay" to the end of it.
-        pigLatinArray.push(index); // take the new string and put it at the end of the pigLatinArray array
-
-      } else {
-        consonantFunction(index);
-        // alert("I'm borked");
-      }
-    // } // closes for loop
-    // for loop - if statement to check letter to see if consonant
-    // true = slice first letter then concate to the end of the string and add ay
-    // false = if a vowel !u add ay then push to array,
-    //determine if last pushed letter is a q
-    // q then slice u and concate to end of string and add ay
-    // else add ay and push to pigLatinArray
-  });
-  return pigLatinArray; // takes the updated pigLatinArray array and sends it to the front-end logic
-}; // closes singleVowelCheck function
-
-
+  var initialArray = string.split(" ");
+  initialArray.forEach(function(index) {
+  if (index[0] === "a" || index[0] === "e" || index[0] === "i" || index[0] === "o" || index[0] === "u" || index.length === 1) {
+    index = index + "ay";
+  pigLatinArray.push(index);
+  } else {
+    consonantFunction(index);
+  }
+});
+  return pigLatinArray;
+};
 function consonantFunction(nonVowelWord) {
   var nonVowelArray = nonVowelWord.split("");
   var consonantArray = [];
-  console.log(nonVowelArray);
-
+  var shiftableArray = nonVowelArray.splice();
+console.log(shiftableArray);
   nonVowelArray.forEach(function(apples){
-  debugger;
-    if (apples != "i") {
-      // apples != "a" && apples != "e" && apples != "i" && apples != "o" && apples != "u") {
-      // var consonantToMove = nonVowelArray.splice(0,1);
-      // console.log(consonantToMove);
-      // consonantArray.push(consonantToMove);
-    } else if (apples === "a"||"e"||"i"||"o"||"u"){
-      console.log("found a vowel");
-      console.log(consonantArray);
+  // debugger;
+    if (apples === "a" || apples ==="e" || apples ==="i" || apples ==="o" || apples ==="u") {
+      console.log("it's a vowel");
+    } else {
+      var consonantToMove = shiftableArray.splice(0,1);
+      console.log(consonantToMove);
+      shiftableArray.push(consonantToMove);
+      console.log(shiftableArray);
     }
   });
-  var checkedWord = nonVowelArray.join("");
+  var checkedWord = shiftableArray.join("");
   checkedWord += "ay";
   pigLatinArray.push(checkedWord);
 };
 
-
 // below this line is user, or front-end, logic
-$(document).ready(function() { //business logic does NOT need to be in doc.ready
-
-
+$(document).ready(function() {
   $("form#userInput").submit(function() {
     event.preventDefault();
     var value = $("#inputString").val();
-    singleVowelCheck(value); // runs the singleVowelCheck function with the input from the user as an argument
+    singleVowelCheck(value);
     $("#result").show();
-    $("#resultP").append(pigLatinArray.join(" ")); //takes translated user input saved as pigLatinArray and joins it back together with spaces, displaying it
+    $("#resultP").append(pigLatinArray.join(" "));
   }) //closes form.submit
 }); //closes doc.ready
