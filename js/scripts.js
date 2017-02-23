b54ff6e0c9baa4821389f1ab2a9577de47e647d9
Dallas Slaughter and Grace Stuart <dallas@>
2017-02-22 16:13:12 -0800

    Remove for loop in vowel function, add new function to move consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,47 @@
 // below this line is business, or back-end, logic
 var pigLatinArray = []; // this is the empty array the translated strings will be entered into
-var consonantsArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]; //array containing all consonants except q, including y
+// var consonantsArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z"]; //array containing all consonants except q, including y
 var vowelsArray = ["a", "e", "i", "o", "u"]; // array containing all vowels except y
 function singleVowelCheck(string) {
   var initialArray = string.split(" "); //takes the input string and makes each item seperated by a space into a single word within the array initialArray
   initialArray.forEach(function(index) { //for each loop that checks every entry in the array created from the user input
-    for (i = 0; i <= vowelsArray.length; i++) {
-      if (index[0] === vowelsArray[i] || index.length === 1) { //if word is single letter, or if word starts with a vowel, do the following function
+    // for (var i = 0; i <= vowelsArray.length; i++) {
+      if (index[0] === "a" || index[0] === "e" || index[0] === "i" || index[0] === "o" || index[0] === "u" || index.length === 1) {
+        //if word is single letter, or if word starts with a vowel, do the following function
         index = index + "ay"; // take the word and add "ay" to the end of it.
         pigLatinArray.push(index); // take the new string and put it at the end of the pigLatinArray array
+
+      } else {
+        console.log(index);
+        consonantFunction(index);
+        // alert("I'm borked");
       }
-    } // closes for loop
+    // } // closes for loop
+    // for loop - if statement to check letter to see if consonant
+    // true = slice first letter then concate to the end of the string and add ay
+    // false = if a vowel !u add ay then push to array,
+    //determine if last pushed letter is a q
+    // q then slice u and concate to end of string and add ay
+    // else add ay and push to pigLatinArray
   });
   return pigLatinArray; // takes the updated pigLatinArray array and sends it to the front-end logic
 }; // closes singleVowelCheck function
+
+function consonantFunction(nonVowelWord) {
+  var nonVowelArray = nonVowelWord.split("");
+  nonVowelArray.forEach(function(){
+
+      if (this != "a" || this != "e" || this != "i" || this != "o" || this != "u") {
+        var consonantToMove = nonVowelArray.shift();
+        console.log(consonantToMove);
+        nonVowelArray.push(consonantToMove);
+      }
+  });
+  var checkedWord = nonVowelArray.join("");
+  checkedWord += "ay";
+  pigLatinArray.push(checkedWord);
+};
+
 
 // below this line is user, or front-end, logic
 $(document).ready(function() { //business logic does NOT need to be in doc.ready
