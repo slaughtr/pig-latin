95427de1ac5f62b1633ba861f5579cded904e515
slaughtr <dallas.xiao@gmail.com>
2017-02-24 16:19:32 -0800

    Add js logic to account for multiple variables, qu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,42 @@
 // below this line is business, or back-end, logic
 var pigLatinArray = [];
 var vowelsArray = ["a", "e", "i", "o", "u"];
+
 function singleVowelCheck(string) {
   var initialArray = string.split(" ");
+
   initialArray.forEach(function(index) {
-  if (index[0] === "a" || index[0] === "e" || index[0] === "i" || index[0] === "o" || index[0] === "u" || index.length === 1) {
-    index = index + "ay";
-  pigLatinArray.push(index);
-  } else {
-    consonantFunction(index);
-  }
-});
+    if (index[0] === "a" || index[0] === "e" || index[0] === "i" || index[0] === "o" || index[0] === "u" || index.length === 1) {
+      index = index + "ay";
+    pigLatinArray.push(index);
+    } else {
+      consonantFunction(index);
+    }
+  });
   return pigLatinArray;
 };
+
 function consonantFunction(nonVowelWord) {
   var nonVowelArray = nonVowelWord.split("");
   var consonantArray = [];
-  var shiftableArray = nonVowelArray.splice();
-console.log(shiftableArray);
-  nonVowelArray.forEach(function(apples){
-  // debugger;
-    if (apples === "a" || apples ==="e" || apples ==="i" || apples ==="o" || apples ==="u") {
-      console.log("it's a vowel");
-    } else {
-      var consonantToMove = shiftableArray.splice(0,1);
-      console.log(consonantToMove);
-      shiftableArray.push(consonantToMove);
-      console.log(shiftableArray);
+  var stop = 0;
+  var index = 0;
+
+  nonVowelArray.forEach(function(letter){
+
+
+    if ((letter === "a" || letter ==="e" || letter ==="i" || letter ==="o" || letter ==="u") && (nonVowelArray[index-1] != "q") && stop === 0) {
+      stop++;
+    } else if (stop === 0){
+      consonantArray.push(nonVowelArray.slice(index, index+1));
     }
+    index++;
   });
-  var checkedWord = shiftableArray.join("");
-  checkedWord += "ay";
-  pigLatinArray.push(checkedWord);
+  consonantArray.unshift(nonVowelArray.slice(consonantArray.length));
+
+  consonantString = [].concat.apply([],consonantArray);
+  console.log(consonantString.join("")+"ay");
+  pigLatinArray.push(consonantString.join("")+"ay");
 };
 
 // below this line is user, or front-end, logic
@@ -42,5 +47,5 @@ $(document).ready(function() {
     singleVowelCheck(value);
     $("#result").show();
     $("#resultP").append(pigLatinArray.join(" "));
-  }) //closes form.submit
-}); //closes doc.ready
+  })
+});
